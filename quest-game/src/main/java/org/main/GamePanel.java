f6656146f7a92e720581c3980671be678c74f98c
jihed-Sh <jihedcr7@gmail.com>
2023-06-26 22:48:55 +0100

    [ADDED] added gameloop with 120 fps , and added the character images

--- quest-game/src/main/java/org/main/GamePanel.java
+++ quest-game/src/main/java/org/main/GamePanel.java
@@ -3,34 +3,61 @@ package org.main;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {
-    private  MouseInputs mouseInput ;
-    private int xDelta =100 , yDelta =100;
+    private MouseInputs mouseInput;
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage image, subImage;
+
     public GamePanel() {
         mouseInput = new MouseInputs(this);
+        importImage();
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInput);
         addMouseMotionListener(mouseInput);
     }
-    public void changeXdelta(int value){
+
+    private void importImage() {
+        try {
+            InputStream in = getClass().getResourceAsStream("/testes.png");
+            assert in != null;
+            image = ImageIO.read(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension screenSize = new Dimension(1400, 800);
+        setPreferredSize(screenSize);
+    }
+
+    public void changeXdelta(int value) {
         this.xDelta += value;
-        repaint();
     }
-    public void changeYdelta(int value){
+
+    public void changeYdelta(int value) {
         this.yDelta += value;
-        repaint();
     }
-    public void setRectPos(int x, int y){
+
+    public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(this.xDelta,this.yDelta,200,50);
+        subImage = image.getSubimage(0*69, 0 * 48, 69, 48);//Depends on the image
+        g.drawImage(subImage, (int) xDelta, (int) yDelta, 128, 80, null);
+
     }
+
 
 }
--- quest-game/src/main/java/org/main/GameWindow.java
+++ quest-game/src/main/java/org/main/GameWindow.java
@@ -4,9 +4,11 @@ public class GameWindow {
     private JFrame  frame;
     public GameWindow(GamePanel panel) {
         frame=new JFrame();
-        frame.setSize(400,400);
+//        frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
+        frame.setResizable(false);
+        frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
